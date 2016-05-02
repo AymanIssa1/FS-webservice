750bb67a0b606db6c306d68b3dc9eb7baf689a6c
Ayman Adel <aymanadel003@gmail.com>
2016-05-02 18:09:53 +0200

    adding company name to register

--- models/customer.js
+++ models/customer.js
@@ -101,7 +101,7 @@ module.exports = function(sequelize, DataTypes) {
         instanceMethods: {
             toPublicJSON: function() {
                 var json = this.toJSON();
-                return _.pick(json, 'id','customerFirstName','customerLastName','isMale', 'email', 'createdAt', 'updatedAt');
+                return _.pick(json, 'id','customerFirstName','customerLastName','companyName','isMale', 'email', 'createdAt', 'updatedAt');
             },
             generateToken: function(type) {
                 if (!_.isString(type)) {
